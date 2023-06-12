aa94ff75ea9e5be10243e37d3246dd92ef386bb4
lee00286 <crocklee1234@gmail.com>
2023-06-12 16:49:08 -0400

    feat: add tests with mocking and spying

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,12 +1,21 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(new Date(2012, 9, 19));
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
 });
 
 test.fails('should match the snapshot', async () => {
-  const { container } = render(<TimeZone />);
+  const { container } = render(<TimeZone getTodos />);
   expect(container).toMatchSnapshot();
 });
